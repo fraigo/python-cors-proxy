41f0652963de73a84e4c12a566d8a73d151da1e3
Francisco <franciscoigor@gmail.com>
2018-12-18 12:35:55 -0800

    Setting to production use

--- corsproxy/settings.py
+++ corsproxy/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#1awt&wk+46=+&(!g!)7empbih+&bjtb21s4u%y_a1x9maqdqk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
