7c4e9ead51ad868b0c687375f62b879213d3d317
Francisco Igor <franciscoigor@gmail.com>
2022-10-25 12:28:04 -0700

    Update views.py

--- corsproxy/views.py
+++ corsproxy/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
     url = parse.quote(request.GET.get('url'))
+    url = url.replace('%3A', ':')
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) Firefox/106.0.1',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*',
